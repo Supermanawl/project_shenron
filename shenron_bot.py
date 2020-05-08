fe4417364887d3a7882882530b5c1e2dd85b6394
Alex Lott <supermanawl@gmail.com>
2020-05-08 17:25:34 -0500

    Shenron can grant a wish!

--- shenron_bot.py
+++ shenron_bot.py
@@ -10,15 +10,12 @@ import shenron_config
 BOT_PREFIX = ("!", "?")
 bot = Bot(command_prefix=BOT_PREFIX)
 
-dragon_balls_collected = 7
-
-
+dragon_balls_collected = 0
 
 # Show Shenron typing...
 async def dragon_type(ctx, seconds):
 	async with ctx.typing():
 		await asyncio.sleep(seconds)
-
 
 
 # Print bot info to console
@@ -30,12 +27,10 @@ async def on_ready():
     await bot.change_presence(activity=Game(name=random.choice(game_library.games)))
 
 
-
 # Fight whoever you'd like for a chance at a dragonball
 @bot.command(pass_context=True)
 async def fight(ctx, name):
 	global dragon_balls_collected
-
 	if bool(random.getrandbits(1)) and dragon_balls_collected < 7:
 		await dragon_type(ctx, 1)
 		await ctx.send('You have defeated ' + name + '!')
@@ -50,23 +45,38 @@ async def fight(ctx, name):
 		await ctx.send('DragonBalls: ' + str(dragon_balls_collected))
 
 
-
+# Respond to the call on shenron!
 @bot.event
 async def on_message(message):
 	global dragon_balls_collected
+	canGrantWish = False
 	channel = message.channel
-
 	if message.author == bot.user:
 		return
 	if message.content.startswith('I CALL ON THE MIGHTY SHENRON!'):
 		if dragon_balls_collected == 7:
-			await dragon_type(channel, 2)
-			await channel.send('{0.author.mention}, What is your wish? You will have only one!'.format(message))
+			while canGrantWish == False:
+				await bot.process_commands(message)
+				await dragon_type(channel, 2)
+				await channel.send('{0.author.mention}, What is your wish? You will have only one!'.format(message))
+				await bot.wait_for('message')
+				if bool(random.getrandbits(1)):
+					await dragon_type(channel, 1)
+					await channel.send("A simple matter...")
+					await dragon_type(channel, 2)
+					await channel.send("Your wish is granted...farewell.")
+					canGrantWish = True
+				else:
+					await dragon_type(channel, 2)
+					await channel.send("I cannot grant a wish that exceeds the power of my creator.")
+					await dragon_type(channel, 1)
+					await channel.send("I'm beginning to grow impatient...")
+			dragon_balls_collected = 0
 		else:
 			await dragon_type(channel, 2)
 			await channel.send("You have not proven yourself to be worthy of my power...")
-
 	await bot.process_commands(message)
+
 
 # Run the bot
 bot.run(shenron_config.token)

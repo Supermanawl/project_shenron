38a2b4a646c29c6f3324c87e7f76eef836aaf7ee
Alex Lott <supermanawl@gmail.com>
2020-05-11 17:25:09 -0500

    Deprecate old shenron_bot and add new games

--- game_library.py
+++ game_library.py
@@ -1,3 +1,5 @@
 games = ["NBA 2K18", "Portal 2", "Grand Theft Auto V", "Minecraft", "Monster Hunter: World",
-"Dragon Ball FighterZ", "Dark Souls", "The Witcher 3: Wild Hunt", "Fallout 4", "The Elder Scrolls V: Skyrim",
-"Overwatch", "Final Fantasy XV", "Civilization VI", "Street Fighter V", "Final Fantasy XIV", "MORDHAU"]
+"Dragon Ball FighterZ", "DARK SOULS™: Prepare To Die Edition", "The Witcher 3: Wild Hunt", "Fallout 4", "The Elder Scrolls V: Skyrim",
+"Overwatch", "Final Fantasy XV", "Civilization VI", "Street Fighter V", "Final Fantasy XIV", "MORDHAU",
+"Stardew Valley", "Tom Clancy's Rainbow Six Siege", "The Witcher 3: Wild Hunt", "Tabletop Simulator",
+"League of Legends", "World of Warcraft Classic"]
